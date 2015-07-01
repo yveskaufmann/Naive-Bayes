7462614cca82cd946def3cbc5d4a41374c3c2c54
Yves Kaufmann <fxdapokalypse@googlemail.com>
2015-07-01 10:26:47 +0200

    Add a proper output

--- main.py
+++ main.py
@@ -9,11 +9,17 @@ pool = Pool()
 for documentClass in documentClasses:
     learnDirectory = join(base, documentClass, "train")
     pool.learn(learnDirectory, documentClass)
-
+documentTags = {}
 for documentClass in documentClasses:
     testDirectory = join(base, documentClass, "test")
     dir = listdir(testDirectory)
     for file in dir:
-        res = pool.Probability(join(testDirectory, file))
-        print(documentClass + ": " + file + ": " + str(res))
+        probabilities = pool.Probability(join(testDirectory, file))
+        probability = sorted(probabilities, reverse=True, key=lambda p: p[1])[0]
+        documentTags[file] = str(probability)
+
+
+for file, tag in sorted(documentTags.items()):
+    print(file + " : " + str(tag))
+
 
